bbc8dc90d391847ea6e2e29db4b33f9f8d605abe
alirezakohandani <alireza.ko73@gmail.com>
2021-03-13 10:55:57 +0330

    Refactoring request class

--- App/Core/Request.php
+++ App/Core/Request.php
@@ -6,58 +6,97 @@ use APP\Utilities\Input;
 
 class Request 
 {
+    /**
+     * Request parameter variable
+     *
+     * @var string
+     */
+    public $params;
+    
+    /**
+     * Request method variable
+     *
+     * @var string
+     */
     public $method;
-    public $uri;
-    public $ip;
-    public $agent;
-    public $referer;
-    public $params; // $_REQUEST
 
-   
+    /**
+     * Request uri variable
+     *
+     * @var string
+     */
+    public $uri;
+
+    /**
+     * Request ip variable
+     *
+     * @var string 
+     */
+    public $ip;
+
+    /**
+     * Request agen variable
+     *
+     * @var string
+     */
+    public $agent;
+
+    /**
+     * Request referer variable
+     *
+     * @var string
+     */
+    public $referer;
+    
+    /**
+     * Set the value for request properties
+     * 
+     * @return mixed
+     */
     public function __construct()
     {
        if (SANITIZE_ALL_DATA) {
            $this->params = Input::clean($_REQUEST);
        } else {
-        $this->params = $_REQUEST;
+           $this->params = $_REQUEST;
        }
        
-//         $keys = array_keys($this->params);
-//          foreach($keys as $key) {
-//               $this->{$key} = $this->param($key);
-//              }
         $this->method = strtolower($_SERVER['REQUEST_METHOD']);
         $this->uri = $_SERVER['REQUEST_URI'];
         $this->ip = $_SERVER['REMOTE_ADDR'];
         $this->agent = $_SERVER['HTTP_USER_AGENT'];
         $this->referer = $_SERVER['HTTP_REFERER'] ?? '';
-      
     }
-    //in function gharae index va kelide arraye request ra begiorad va meghdaro bargardanad
+
+    /**
+     * Checks if there is a property in the Request parameters
+     *
+     * @param string $key
+     * @return boolean
+     */
     public function key_exists($key) {
         return in_array($key, array_keys($this->params));
     }
+
     public function is_in($methods_arr) {
         return in_array($this->method, $methods_arr);
     }
+
     public function param($key) {
         return $this->params[$key];
     }
 
-    // public function __set($key, $value)
-    // {
-    //     echo "this is new property";
-    // }
-
+    /**
+     * Set the value to a property that does not exist
+     *
+     * @param string $key
+     * @return void
+     */
     public function __get($key)
     {
-        if ($this->key_exists($key)) {
-            return $this->{$key} = $this->param($key);
+        if (!$this->key_exists($key) && IS_DEV_MODE) {
+            return "$key property is not exists!";
         } 
-        //for developer -- if dev_mode is on,  send notification for developer
-        else {
-            // notify programmer
-            echo "key is not exists!";
-        }
+            return $this->$key = $this->param($key);
     }
 }
