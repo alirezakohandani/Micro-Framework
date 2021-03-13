4d8ba509c30a94cab9859e1d32a2fb6d0ac9f00c
alirezakohandani <alireza.ko73@gmail.com>
2021-03-13 13:38:05 +0330

    Refactoring router class

--- App/Services/Router/Router.php
+++ App/Services/Router/Router.php
@@ -5,49 +5,44 @@ namespace App\Services\Router;
 use App\Core\Request;
 use App\Services\View\View;
 
-class Router{
+class Router
+{
 
     private static $routes;
     const baseController = "App\Controllers\\";
     const baseMiddlewares = 'App\Middlewares\\';
 
-   
-
-    public static function start() {
-        // echo "router starts!";
-        // get all routes (load address book)
-        $routes = self::get_all_routes();
-        // check if route exists
+    /**
+     * Start router service
+     *
+     * @return void
+     */
+    public static function start() 
+    {
         $current_uri = self::get_current_route();
-     
+
         if (self::route_exists($current_uri)) {
-            // echo "yes";
-            //refactoring(!)
             $request = new Request();
             $allowed_methods = self::get_route_methods($current_uri);
-         
              if (!$request->is_in($allowed_methods)) {
                 header('HTTP/1.0 403 Forbidden');
                 View::load('errors.403');
                 die();
              }
-
              //chechk middleware
              if (self::has_middleware($current_uri)) {
              $middlewares = self::get_route_middlewares($current_uri);
-             foreach ($middlewares as $middleware) {
-                $middlewareClass = self::baseMiddlewares . $middleware;
-                
-                    if (!class_exists($middlewareClass)) {
-                       echo "Error: Class $middlewareClass was not found!";
-                       die();
+                foreach ($middlewares as $middleware) {
+                    $middlewareClass = self::baseMiddlewares . $middleware;
+                        if (!class_exists($middlewareClass)) {
+                        echo "Error: Class $middlewareClass was not found!";
+                        die();
                     }
-                    $middlewareObj = new $middlewareClass;
-                    $middlewareObj->handle($request);
+                        $middlewareObj = new $middlewareClass;
+                        $middlewareObj->handle($request);
                 }
              }
            
-            
              //call controller method
             $targetStr = self::get_route_target($current_uri);
             list($controller, $method) = explode('@', $targetStr);
@@ -68,7 +63,7 @@ class Router{
         } else {
             header("HTTP/1.0 404 Not Found");
             // View::load('errors/404');
-               view::load('errors.404');
+            view::load('errors.404');
             die();
         }
         // if route not exists : 404.php
@@ -76,32 +71,82 @@ class Router{
         // create an object from target Controller and call the method (ghesmate 1 dakhele shekle loghman)
         //
     }
+
+    /**
+     * Returns all routes
+     *
+     * @return void
+     */
     public static function get_all_routes() {
         return include BASE_PATH . "routes/web.php"; 
     }
+
+    /**
+     * Check the existence of the route
+     *
+     * @param string $route
+     * @return boolean
+     */ 
     public static function route_exists($route) {
         $routes = self::get_all_routes();
         return in_array($route, array_keys($routes));
     }
+
+    /**
+     * Returns request target
+     *
+     * @param [type] $route
+     * @return void
+     */
     public static function get_route_target($route) {
         $routes = self::get_all_routes();
         return $routes[$route]['target'];
     }
+
+    /**
+     * Returns current route
+     *
+     * @return void
+     */
     public static function get_current_route() {
         $current_uri  = str_replace(SUB_DIRECTORY, '', $_SERVER['REQUEST_URI']);
         return strtok($current_uri, '?');
     }
-    public static function get_route_methods ($route) {
+
+    /**
+     * Returns request method
+     *
+     * @param string $route
+     * @return void
+     */
+    public static function get_route_methods($route) {
         $routes = self::get_all_routes();
-        $methods_str =  $routes[$route]['method'];
+        $methods_str =  strtolower($routes[$route]['method']);
         return explode('|', $methods_str);
+    }
+
+    /**
+     * Returns request middleware
+     *
+     * @param string $route
+     * @return void
+     */
+    public static function get_route_middlewares($route) {
+        $routes = self::get_all_routes();
+        if (array_key_exists('middleware', $routes[$route])) {
+            $middlewareStr = GLOBAL_MIDDLEWARES . "|" . ($routes[$route]['middleware'] ?? '');
+            return removeEmptyMembers(explode('|', $middlewareStr));
+        }
+            return null;
         
     }
-    public static function get_route_middlewares($route) {
-      $routes = self::get_all_routes();
-      $middlewareStr = GLOBAL_MIDDLEWARES . "|" . ($routes[$route]['middleware'] ?? '');
-      return removeEmptyMembers(explode('|', $middlewareStr));
-    }
+
+    /**
+     * Checks middleware exists
+     *
+     * @param string $route
+     * @return boolean
+     */
     public static function has_middleware ($route) {
         $routes = self::get_all_routes();
         return isset($routes[$route]['middleware']) or !empty(GLOBAL_MIDDLEWARES);
--- App/Utilities/Input.php
+++ App/Utilities/Input.php
@@ -8,12 +8,12 @@ class Input {
      * Clean the input data
      *
      * @param string $data
-     * @return string
+     * @return $data
      */
     public static function clean($data) {
         // data cleaner 
         $data = stripslashes($data);
-        $data = strip_tags($data);
+        $data = strip_tags('<script></script>');
         $data = filter_var($data, FILTER_SANITIZE_MAGIC_QUOTES);
         return $data;
     }
