ea2d04372f4606cd43f968f7a327b98e72abd343
alirezakohandani <alireza.ko73@gmail.com>
2021-03-14 12:35:01 +0330

    Create user model

---
+++ App/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Contract\BaseModel;
+
+class User extends BaseModel
+{
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    public static $table = 'users';
+
+    
+}
